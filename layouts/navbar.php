c95ca224ec977a596b30ed29797f50c60d810f90
lutfialfasha <lutfialfasha05@gmail.com>
2023-08-15 10:11:09 +0800

    Memperbaiki Tampilan

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,8 +1,12 @@
+<?php
+   $baseUrl = 'http://localhost/datamagang/' 
+?>
 <nav>
      <ul>
-        <li><a href="datasiswa/index.php">Data Siswa Magang</a></li>
-        <li><a href="datatempat/index2.php">Data Tempat Magang</a></li>
-        <li><a href="input.php">Tambah Data Siswa</a></li>
-        <li><a href="inputM.php">Tambah Data Tempat Magang</a></li>
+        <li><a href="<?= $baseUrl?>">Beranda</a></li>
+        <li><a href="<?= $baseUrl?>datasiswa/index.php">Data Siswa Magang</a></li>
+        <li><a href="<?= $baseUrl?>datatempat/index.php">Data Tempat Magang</a></li>
+        <li><a href="<?= $baseUrl?>datasiswa/input.php">Tambah Data Siswa</a></li>
+        <li><a href="<?= $baseUrl?>datatempat/inputm.php">Tambah Data Tempat Magang</a></li>
      </ul>
 </nav>
