52f57d2083871a7e8f7bd445a82fe85925ad3e6e
lutfialfasha <lutfialfasha05@gmail.com>
2023-08-15 12:00:24 +0800

    menambahkan Boostrap

--- index.php
+++ index.php
@@ -1,47 +1,12 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Data Magang</title>
-    <style>
-     * {
-         margin:0; 
-         padding:0;
-     }
- 
-    nav {
-        font-family: arial;
-    } 
-    
-    nav ul {
-        background:violet;
-        padding: 0 20px;
-        list-style: none;
-        display: inline-table;
-        width: 100%;
-     }
-
-    nav ul li{
-        float:left;
-    }
-
-    nav ul li:hover{
-        background:white;
-    }
-
-    nav ul li:hover a{
-        color:blue;
-    }
-
-    nav ul li a{
-        display: block;
-        padding: 25px;
-        color: purple;
-        text-decoration: none;
-    }
-    </style>
-
 </head>
-<body style="background: lavender;">
 <?php include 'layouts/navbar.php'; ?>
 <h3 align="center">SELAMAT DATANG</h3>
 </td>
